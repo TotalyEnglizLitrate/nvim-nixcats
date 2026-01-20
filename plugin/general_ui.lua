2bd5d9668c05f6518a0b702c60c78bd5cc1acb8b
Narendra S <narendra.s1232@gmail.com>
2026-01-20 09:20:00 +0530

    uncomment noice config

--- plugin/general_ui.lua
+++ plugin/general_ui.lua
@@ -72,45 +72,45 @@ require('lualine').setup({
 })
 
 
--- require('noice').setup({
---   presets = {{ bottom_search = true }},
---   views = {
---     cmdline_popup = {
---       position = {
---         row = -2,
---         col = '50%',
---       },
---       win_options = {
---         winblend = 0,
---       }
---     },
---     cmdline_popupmenu = {
---       position = {
---         row = -5,
---         col = '50%'
---       },
---       win_options = {
---         winblend = 0,
---       }
---     },
---     mini = {
---       win_options = {
---         winblend = 0,
---       }
---     }
---   },
---   lsp = {
---     override = {
---       ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
---       ['vim.lsp.util.stylize_markdown'] = true,
---       ['cmp.entry.get_documentation'] = true,
---     },
---     hover = { enabled = false },
---     signature = { enabled = false },
---     message = { enabled = false },
---     progress = { enabled = false },
---   }
--- })
+require('noice').setup({
+  presets = {{ bottom_search = true }},
+  views = {
+    cmdline_popup = {
+      position = {
+        row = -2,
+        col = '50%',
+      },
+      win_options = {
+        winblend = 0,
+      }
+    },
+    cmdline_popupmenu = {
+      position = {
+        row = -5,
+        col = '50%'
+      },
+      win_options = {
+        winblend = 0,
+      }
+    },
+    mini = {
+      win_options = {
+        winblend = 0,
+      }
+    }
+  },
+  lsp = {
+    override = {
+      ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+      ['vim.lsp.util.stylize_markdown'] = true,
+      ['cmp.entry.get_documentation'] = true,
+    },
+    hover = { enabled = false },
+    signature = { enabled = false },
+    message = { enabled = false },
+    progress = { enabled = false },
+  }
+})
 
 vim.cmd([[hi StatusLine guibg=NONE ctermbg=NONE]])
 
